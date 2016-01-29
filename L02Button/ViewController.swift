9f18bd7be75bd5a6ae54eb2c93f65568c7f8a3c7
Katie Riley <katie.ann.riley@hotmail.com>
2016-01-29 12:34:51 -0800

    Initial Commit

---
+++ L02Button/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  L02Button
+//
+//  Created by Kathryn Riley on 1/29/16.
+//  Copyright © 2016 ___KathrynHoskins___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
