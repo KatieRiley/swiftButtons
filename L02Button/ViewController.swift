83990f791b7507cf8d0a6a3c6ed98dc39585d4c0
Katie Riley <katie.ann.riley@hotmail.com>
2016-02-01 15:20:10 -0800

    Finished project

--- L02Button/ViewController.swift
+++ L02Button/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    var leftButtonCount = 0
+    var rightButtonCount = 0
+    
+    
+    @IBAction func doLeftButton(sender: AnyObject) {
+        leftButtonCount++
+        let text = "Left Button Pressed " + String(leftButtonCount) + " times"
+        
+        let attributedText = NSMutableAttributedString(string: text)
+        
+        let attributes = [NSFontAttributeName: UIFont.boldSystemFontOfSize(myLabel.font.pointSize)]
+        
+        let newRange = (text as NSString).rangeOfString(text)
+        attributedText.setAttributes(attributes, range: newRange)
+        
+        myLabel.attributedText = attributedText
+        
+    }
+    @IBAction func doRightButton(sender: AnyObject) {
+        rightButtonCount++
+        myLabel.text = "Right Button Pressed " + String(rightButtonCount) + " times"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
